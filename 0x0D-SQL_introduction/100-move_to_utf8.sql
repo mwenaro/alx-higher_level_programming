fac9aa3b17b590a83e7a918a4be0e3070adab2ee
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-09-08 16:25:01 -0700

    Bug-fix in file 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,14 +6,12 @@ CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_unicode_ci;
 
 -- Convert table 'first_table'
-USE hbtn_0c_0
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
 -- Convert field 'name' in 'first_table'
-USE hbtn_0c_0;
-ALTER TABLE first_table
+ALTER TABLE hbtn_0c_0.first_table
 MODIFY name
 VARCHAR(256)
 CHARACTER SET utf8mb4
