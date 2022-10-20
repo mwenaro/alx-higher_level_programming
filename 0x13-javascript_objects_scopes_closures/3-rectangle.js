b90a5372b09fe0a12541e6a2980d261dfb6f4953
Mwero Abdalla <mwerothewebmaker@gmail.com>
2022-10-20 06:53:01 +0300

    Bug -fix

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -2,20 +2,20 @@
 class Rectangle {
   constructor (w, h) {
     if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
+      this.width = w
+      this.height = h
     }
   }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      let s = '';
+      let s = ''
       for (let j = 0; j < this.width; j++) {
-        s += 'X';
+        s += 'X'
       }
-      console.log(s);
+      console.log(s)
     }
   }
 }
 
-module.exports = Rectangle;
+module.exports = Rectangle
