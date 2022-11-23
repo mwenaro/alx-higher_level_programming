d2c56925ae38fa103106be08da41953aad24ee58
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-11-23 19:17:24 +0300

    Update 100-script.js

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,5 +1,3 @@
-// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000)
-
-const header = document.querySelector('header');
-
-header.style.color = '#FF0000';
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelector('header').style.color = '#FF0000';
+});
