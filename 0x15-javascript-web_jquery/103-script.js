e732dea5ae49eb58c584dfe52f383957f18d819d
MWERO ABDALLA <mwerothewebmaker@gmail.com>
2022-11-23 19:12:02 +0300

    Update 103-script.js

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,29 +1,8 @@
-//
-const btnTranslate = document.querySelector('INPUT#btn_translate');
-const languageCode = document.querySelector('INPUT#language_code');
-const helloDiv = document.querySelector('DIV#hello');
-
-btnTranslate.addEventListener('click', () => {
-  // Get the language code
-  const code = languageCode.value;
-  fetch(`https://stefanbohacek.com/hellosalut/?lang=${code}`)
-    .then((res) => res.json())
-    .then(({ hello }) => {
-      helloDiv.innerHTML = hello;
-    })
-    .catch((err) => console.log(' Something went wrong, ' + err));
-});
-
-// on Enter
-languageCode.addEventListener('keydown', (e) => {
-  if (['Enter', 'NampadEnter'].includes(e.code)) {
-    // Get the language code
-    const lanCode = languageCode.value;
-    fetch(`https://stefanbohacek.com/hellosalut/?lang=${lanCode}`)
-      .then((res) => res.json())
-      .then(({ hello }) => {
-        helloDiv.innerHTML = hello;
-      })
-      .catch((err) => console.log(' Something went wrong, ' + err));
-  }
+$('document').ready(function () {
+  const url = 'https://www.fourtonfish.com/hellosalut/?';
+  $('INPUT#btn_translate').click(function () {
+    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
+      $('DIV#hello').html(data.hello);
+    });
+  });
 });
